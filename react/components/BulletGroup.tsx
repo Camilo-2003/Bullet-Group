e456e4aec1a3d26150b39247888c5e1973fd8d9e
Camilo_Muñoz <juachomanco@gmail.com>
2025-11-25 08:22:03 -0500

    updating my BulletGroup component

--- react/components/BulletGroup.tsx
+++ react/components/BulletGroup.tsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
+import { BulletGroupProp } from "../interfaces/Bulletsinterfaces";
+import { BulletMap } from "./BulletMap";
+import { useCssHandles } from "vtex.css-handles";
+import { useListContext, ListContextProvider } from "vtex.list-context";
+import { useDevice } from "vtex.device-detector";
 
-const BulletGroup = () => {
+const BulletGroup = ({ bullets, children }: PropsWithChildren<BulletGroupProp>) => {
+  const { list } = useListContext() || [];
+  const { isMobile } = useDevice();
+
+  const CSS = [
+      "bullet__container",
+      "bullet__container--title"
+    ];
+    
+  const handle = useCssHandles(CSS);
+
+  const bulletsGroup = BulletMap(bullets);
+
+  const newBulletsGroup = list.concat(bulletsGroup);
+
   return (
-    <>
-    HOLA DESDE EL BULLET GROUP :)
-    </>
+    <ListContextProvider list={newBulletsGroup}>
+      {
+        isMobile ? 
+        <div>
+          <h3 className={handle["bullet__container--title"]}>Futuras Categorias</h3>
+          <section className={handle.bullet__container}>
+            {bulletsGroup}
+          </section>
+        </div>
+      :
+        children
+      }
+    </ListContextProvider>
   )
 
 }
---
+++ react/components/BulletMap.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BulletsSchema, BulletSchema } from "../typings/BulletsType";
+import Bullet from "./Bullet";
+
+export const BulletMap = (bullets: BulletsSchema) => {
+  const emptyLink = {
+    url: "",
+    attributeNofollow: false,
+    attributeTitle: "",
+    openNewTab: false,
+    newTab: false
+  }
+
+  return bullets.map((bullet: BulletSchema, index) => {
+    return (
+      <Bullet 
+        key={index}
+        image={bullet.image}
+        title={bullet.title}
+        link={(bullet.link ? bullet.link : emptyLink)}
+       />
+    )
+  });
+}
